c377cf857b6fe739c774c3b2c7c8ac02e7ef9330
Franco <coria9404@gmail.com>
2026-05-29 11:58:31 -0300

    "conexion a database"

--- backend-esueldos-calculadoras/src/db.js
+++ backend-esueldos-calculadoras/src/db.js
@@ -1,31 +1,39 @@
 const { MongoClient } = require("mongodb");
-
-const uri = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017";
+ 
+const uri = process.env.MONGODB_URI;
 const dbName = process.env.MONGODB_DB || "esueldos_calculadoras";
-
+ 
 let clientPromise;
-
+ 
 async function getClient() {
   if (!clientPromise) {
-    const client = new MongoClient(uri);
+    const client = new MongoClient(uri, {
+      tls: true,
+      tlsInsecure: true,
+    });
+ 
     clientPromise = client.connect();
   }
+ 
   return clientPromise;
 }
-
+ 
 async function getDb() {
   const client = await getClient();
   return client.db(dbName);
 }
-
+ 
 async function closeDb() {
   if (!clientPromise) return;
+ 
   const client = await clientPromise;
+ 
   await client.close();
+ 
   clientPromise = null;
 }
-
+ 
 module.exports = {
   getDb,
-  closeDb
+  closeDb,
 };
